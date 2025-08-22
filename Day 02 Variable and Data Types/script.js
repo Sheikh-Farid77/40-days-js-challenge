e0ef14dc0fefa98a8d0104c7872208baea75a842
Sheikh Farid <mdfaridsagor101@gmail.com>
2025-08-22 11:13:21 +0600

    complete day 02 challenge

---
+++ Day 02 Variable and Data Types/script.js
@@ -0,0 +1,30 @@
+// variable are used to store data in javascript
+// variable is just like a box
+
+let fruit = "mango";
+fruit = "banana";
+let vegetable = 'carrot';
+
+fruit = vegetable;
+
+console.log(fruit);
+
+// it is the example of pass by value and it only work in primitive data types
+
+// There are 2 types of value in javascript 
+//1. primitive
+// number, string, boolean, null, undefined, symbol, Bigint
+// it save on stack memory
+
+// 2. non-primitive
+// array, function, object
+// it save in heap memory
+
+
+// how javascript see code
+
+// 1. tokenizing
+// 2. parsing
+// 3. interpreting
+
+// source code -> tokenizing -> parsing -> abstract syntax tree (AST) -> machine code
